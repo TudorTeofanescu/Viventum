18d40e07c9357ebd25db275941b18bb3d5eab6b9
Tudor Teofanescu <tudorteofanescu@yahoo.com>
2022-04-25 23:23:50 +0200

    apl code

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,20 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "viventum": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "auth": {
+        "viventum65dbeb3b": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
--- src/index.js
+++ src/index.js
@@ -4,11 +4,27 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// aws
+import { Amplify } from 'aws-amplify';
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
+
+// from v2
+// import { MoralisProvider } from "react-moralis";
+// import "./index.css";
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  // <MoralisProvider
+  //   serverUrl="https://c76movz00vcz.usemoralis.com:2053/server"
+  //   appId="Su5msyzoLMESzSbgODX3FqpKUMCifryZvKQ7oeCe"
+  // >
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  // </MoralisProvider>,
+  // document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
